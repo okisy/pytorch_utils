22fe773f63ec5e94c979d8f71b87491403a6205e
Sho Maeoki <32443681+okisy@users.noreply.github.com>
2020-07-10 20:34:12 +0900

    Create model_utils.py

---
+++ model_utils.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+
+# layers = []
+# for i in range(5):
+#     layers.append(nn.Linear(32, 32))
+# module = nn.Sequential(*layers)
+# print(module)
+
+
+# module = torch.nn.Sequential()
+# for i in range(5):
+#     module.add_module("linear_"+str(i), nn.Linear(32, 32))
+# print(module)
